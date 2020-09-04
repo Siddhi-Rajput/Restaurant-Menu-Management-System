9dab5edca4994980509fc65ec8e0cab29f9d3ece
Siddhi Rajput <siddhirajput227@gmail.com>
2020-09-05 02:30:26 +0530

    Updates

---
+++ precache-manifest.90949823e74ad2b39bc400383fea5261.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "944bd16ab86090e33b9968ba0625d3cd",
+    "url": "/Restaurant-Menu-Management-System/index.html"
+  },
+  {
+    "revision": "80ed049d761d6655c459",
+    "url": "/Restaurant-Menu-Management-System/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "2b94b3c2cbb0192bb5a3",
+    "url": "/Restaurant-Menu-Management-System/static/css/main.58f310c1.chunk.css"
+  },
+  {
+    "revision": "80ed049d761d6655c459",
+    "url": "/Restaurant-Menu-Management-System/static/js/2.15cb6406.chunk.js"
+  },
+  {
+    "revision": "4e5653c2aea1f8f01a4afdf4252ee778",
+    "url": "/Restaurant-Menu-Management-System/static/js/2.15cb6406.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b94b3c2cbb0192bb5a3",
+    "url": "/Restaurant-Menu-Management-System/static/js/main.a90e389f.chunk.js"
+  },
+  {
+    "revision": "9a59aab485c63c52fb29",
+    "url": "/Restaurant-Menu-Management-System/static/js/runtime-main.80f13e4c.js"
+  },
+  {
+    "revision": "ea0b3892a409719c22d50d3edfbbdaf3",
+    "url": "/Restaurant-Menu-Management-System/static/media/food2.ea0b3892.jpg"
+  },
+  {
+    "revision": "9c349ff5dc6b179aa640b0542711f066",
+    "url": "/Restaurant-Menu-Management-System/static/media/logo.9c349ff5.svg"
+  }
+]);
